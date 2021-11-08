d2b211a24c18908cb26f759e8ff7854f3d701861
eugeneklymenko <zekan.klymenko@gmail.com>
2021-11-08 10:57:40 +0200

    #0

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -1,4 +1,12 @@
 package com.codurance;
 
 public class StringCalculator {
+
+    public int Add(String input) {
+        if(input.length() == 0) {
+            return 0;
+        }
+        return Integer.parseInt(input.split(",")[0].trim())
+                + Integer.parseInt(input.split(",")[1].trim());
+    }
 }
--- src/test/java/com/codurance/StringCalculatorShould.java
+++ src/test/java/com/codurance/StringCalculatorShould.java
@@ -1,9 +1,29 @@
 package com.codurance;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 public class StringCalculatorShould {
+
+    private StringCalculator stringCalculator;
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value ={
+            "''; 0",
+            "0, 0; 0",
+            "1, 0; 1",
+            "1, 2; 3",
+            "1, 2, 3, 4, 5, 6, 7, 8, 9; 45"
+    }, delimiter = ';')
+    void returns_correct_output_with_input(String input, int output) {
+        Assertions.assertEquals(output, stringCalculator.Add(input));
+    }
 
 }
