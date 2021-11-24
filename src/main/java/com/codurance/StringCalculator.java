901d3ab384911e205261a8d70f54f2045f532a2c
eugeneklymenko <zekan.klymenko@gmail.com>
2021-11-24 05:36:09 +0200

    Step #3

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -3,17 +3,27 @@ package com.codurance;
 public class StringCalculator {
 
     public int Add(String input) {
-        int sum = 0;
+
         if(input.length() == 0) {
             return 0;
+        } else {
+            String[] numList = SplitUndefinedNumbers(input);
+            return sum(numList);
         }
-        String[] numbers = input.split(",".trim());
+    }
 
-        for (int i =0; i < numbers.length; i++) {
-            int number = Integer.parseInt(numbers[i].replaceAll("[^0-9]", ""));
+    private static String[] SplitUndefinedNumbers(String input) {
+        return input.split(("[\\r\\n|,]").trim());
+    }
+
+    private static int sum(String[] numbers) {
+        int sum = 0;
+
+        for (String s : numbers) {
+            int number = Integer.parseInt(s.replaceAll("[^0-9]", ""));
             sum += number;
         }
-        System.out.println(sum);
         return sum;
     }
+
 }
