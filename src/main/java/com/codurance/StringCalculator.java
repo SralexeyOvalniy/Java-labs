b8774df0ade9c58d567779d811d21d61fe497d7d
eugeneklymenko <zekan.klymenko@gmail.com>
2021-11-24 04:25:00 +0200

    #2

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -3,10 +3,17 @@ package com.codurance;
 public class StringCalculator {
 
     public int Add(String input) {
+        int sum = 0;
         if(input.length() == 0) {
             return 0;
         }
-        return Integer.parseInt(input.split(",")[0].trim())
-                + Integer.parseInt(input.split(",")[1].trim());
+        String[] numbers = input.split(",".trim());
+
+        for (int i =0; i < numbers.length; i++) {
+            int number = Integer.parseInt(numbers[i].replaceAll("[^0-9]", ""));
+            sum += number;
+        }
+        System.out.println(sum);
+        return sum;
     }
 }
